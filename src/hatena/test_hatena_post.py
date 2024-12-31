11dfdd10d4e0d4e077af00f6f657b95512500773
rindguitar <icchii80241@gmail.com>
2025-01-01 00:39:44 +0900

    hatena test

--- src/hatena/test_hatena_post.py
+++ src/hatena/test_hatena_post.py
@@ -3,9 +3,7 @@ from requests.auth import HTTPBasicAuth
 import dotenv
 import os
 
-
 dotenv.load_dotenv()
-
 
 hatena_id = os.getenv("HATENA_ID")
 blog_id = os.getenv("HATENA_BLOG_ID")
@@ -17,12 +15,10 @@ req_endpoint = f"{url}{endpoint}"
 # test_post_data.xmlを読み込む
 with open('config/test_post_data.xml', 'r', encoding='utf-8') as file:
     data = file.read()
-    
-    
-    # POSTリクエストを送信
-    response = requests.post(req_endpoint, auth=HTTPBasicAuth(username=hatena_id, password=key), headers={'Content-Type': 'application/xml'},data=data)
-    
-    
+
+# POSTリクエストを送信
+response = requests.post(req_endpoint, auth=HTTPBasicAuth(username=hatena_id, password=key), headers={'Content-Type': 'application/xml'}, data=data)
+
 if response.status_code == 201:
     print("ok")
 else:
